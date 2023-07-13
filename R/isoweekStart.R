d438d5f5c8c01b5ebd09be9d17e3700eaa14e3a6
Theodore Lytras <thlytras@gmail.com>
2023-07-13 14:20:33 +0300

    Add is.isoweek() and seq_isoweek().
    Also make isoweekStart() return NA and a warning, rather than an error, if invalid ISOweeks provided.

--- R/isoweekStart.R
+++ R/isoweekStart.R
@@ -7,7 +7,7 @@
 #' @param x A numeric vector of ISO week numbers (of format YYYYWW) 
 #'
 #' @return A vector of class \code{\link{Date}} and length equal to \code{x}, containing the 
-#'   start date (first Monday) of each ISO week. 
+#'   start date (first Monday) of each ISO week. \code{NA} (with a warning) if ISO week is invalid.
 #'
 #' @examples
 #' isoweek(201740) # Start of 2017-18 influenza surveillance
@@ -23,6 +23,66 @@ isoweekStart <- function(x) {
   x.nearest.thu <- x.date-x.weekday+4
   x.isoweek <- isoweek(x.nearest.thu, type="week")
   res <- x.nearest.thu + 7*(week-x.isoweek) - 3
-  if (sum(isoweek(res, type="both_num") != x)>0) stop("Error specifying ISO week number")
+  if (sum(is.na(res)) > 0) 
+    warning("Invalid ISO week numbers provided")
   return(res)
+}
+
+
+
+#' Are values valid ISO weeks?
+#'
+#' Take a numeric vector and return \code{TRUE} if values are valid ISO week numbers (of the
+#'   form YYYYWW), or \code{FALSE} otherwise.
+#'
+#' @param x A numeric vector of ISO week numbers (of format YYYYWW) 
+#'
+#' @return A logical vector of the same length as \code{x}
+#'
+#' @examples
+#' is.isoweek(c(202053, 202153))  # returns c(TRUE, FALSE)
+#'
+#' @export
+is.isoweek <- function(x) {
+  !is.na(suppressWarnings(isoweekStart(x)))
+}
+
+
+
+#' Generate regular sequences of ISO weeks
+#'
+#' Take two ISO week numbers (of the form YYYYWW) and returns a regular sequence of 
+#' ISO weeks covering the entire range.
+#'
+#' @param from Numeric vector of length 1 (first week of sequence), or of length 2
+#'   (week range) if \code{to} is \code{NULL}
+#' @param to Numeric vector of length 1 (last week of sequence), or \code{NULL} (in which
+#'   case \code{from} must be a range of weeks).
+#'
+#' @return A numeric vector of ISO weeks covering the provided range.
+#'
+#' @examples
+#' seq_isoweek(c(202001, 202301))
+#' seq_isoweek(201840, 201920)  # Typical influenza surveillance period
+#'
+#' @export
+seq_isoweek <- function(from, to=NULL) {
+  if (is.null(to)) {
+    to <- from[2]
+    from <- from[1]
+  }
+  if (length(from)>1) {
+    from <- from[1]
+    warning("Argument 'from' has length >1 and only the first element will be used")
+  }
+  if (length(to)>1) {
+    to <- to[1]
+    warning("Argument 'to' has length >1 and only the first element will be used")
+  }
+  from <- suppressWarnings(isoweekStart(from))
+  to <- suppressWarnings(isoweekStart(to))
+  if (is.na(from) || is.na(to))
+    stop("Invalid ISO weeks supplied")
+  res <- seq.Date(from, to, by="week")
+  isoweek(res)
 }
